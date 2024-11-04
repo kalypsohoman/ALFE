eb6c09ea99436609b082ab0a2d4930375269989b
kalypsohoman <catcatmcgee@gmail.com>
2024-11-04 09:30:14 -0600

    Add layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,60 @@
+<script>
+</script>
+
+<nav>
+    <h1>My App</h1>
+</nav>
+
+<slot />
+
+
+<style lang='scss'>
+    nav {
+        height: 80px;
+        border-bottom: 1px solid white;
+        display: flex;
+        align-items: center;
+        
+        a:first-child {
+            margin-right: auto; // Pushes the first child to the left and others to the right
+            img {
+                width: 200px;
+                margin: 10px;
+            }
+        }
+        .links {
+            display: flex;
+            align-items: flex-end;
+            height: 80px;
+            a {
+                color: white;
+                text-decoration: none;
+                display: flex;
+                font-family: "Mourgen", sans-serif;
+                font-size: 3rem;
+                margin-inline: 10px;
+                margin-bottom: 3px;
+                &:not(.active):hover {  //only applies the hover color change if the class isn't active
+                    color: orange;
+                }
+            }
+            p{
+                display: contents;
+                font-size: 4.5rem;
+            }
+
+            .active {
+                border-top: solid orange;
+                border-bottom: solid orange;
+                cursor: default;
+            }
+        }
+
+        // MOBILE STYLING
+        @media (max-width: 850px) {
+            .links {
+                display: none;
+            }
+        }
+    }
+</style>

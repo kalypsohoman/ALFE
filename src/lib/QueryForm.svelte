405eb696e6a7bb8eb434ccf9e0018484ad0559c6
kalypsohoman <catcatmcgee@gmail.com>
2024-11-04 16:49:32 -0600

    QueryForm hello world

---
+++ src/lib/QueryForm.svelte
@@ -0,0 +1,20 @@
+<script>
+    // @ts-nocheck
+    import { store } from '../stores';
+
+    $: messages = $store.messages;
+    function handleSubmit(event, formData = new FormData(event.target)) {
+        event.preventDefault();
+        
+        const query = formData.get('query');
+        store.update(current => ({
+            ...current,
+            messages: [...current.messages, query]
+        }));
+    }
+</script>
+
+<form on:submit={handleSubmit}>
+    <input type="text" name="query" placeholder="Ask Chronicle...">
+    <button type="submit">Submit</button>
+</form>
